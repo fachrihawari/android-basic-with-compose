1e8b36e57f0f9afbf14a4dfe32cb9966f604321c
Fachri Hawari <fachri.hawari@gmail.com>
2023-05-30 17:19:59 +0700

    affirmation project

---
+++ 04_Affirmations/app/src/main/java/dev/hawari/affirmations/MainActivity.kt
@@ -0,0 +1,87 @@
+package dev.hawari.affirmations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.hawari.affirmations.data.Datasource
+import dev.hawari.affirmations.model.Affirmation
+import dev.hawari.affirmations.ui.theme.AffirmationsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AffirmationsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AffirmationApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AffirmationApp() {
+    AffirmationList(affirmationList = Datasource().loadAffirmations())
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Text(
+            text = stringResource(affirmation.stringResourceId),
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.headlineSmall
+        )
+    }
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) {
+            AffirmationCard(affirmation = it, modifier = Modifier.padding(8.dp))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AffirmationCardPreview() {
+    AffirmationsTheme {
+        AffirmationCard(affirmation = Affirmation(R.string.affirmation1, R.drawable.image1))
+    }
+}
